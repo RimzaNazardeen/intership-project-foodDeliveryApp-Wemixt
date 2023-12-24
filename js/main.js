03fb0b69175ffae3a579c020ed2d946c30771001
RimzaNazardeen <rimzanazardeen@gmail.com>
2023-12-24 09:17:58 +0530

    Add new hover effect navigation bar : Added a hover effect to the navigation bar to enhance user interaction with the navigation bar. when user hover over each navigation item, the text color transition smoothly and an overline is displayed. This enhancement provides a distinct visual cue , improving the experience.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,32 @@ let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick  = ()=>{
-    menu.classList.toggle('fa-times');
+    menu.classList.toggle('ri-close-fill');
     navbar.classList.toggle('active');
 }
+
+// let section = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll  = ()=>{
+    menu.classList.remove('ri-close-fill');
+    navbar.classList.remove('active');
+
+    // section.forEach(sec =>{
+    //     let top = window.scrollY;
+    //     let height = sec.offsetHeight;
+    //     let offset = sec.offsetTop - 150;
+    //     let id = sec.getAttribute('id');
+
+    //     if(top => offset && top < offset + height){
+    //         navLinks.forEach(links =>{
+    //             links.classList.remove('active');
+    //             document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+    //         });
+    //     };
+    // });
+}
+
+
+
 //Menu bar end
